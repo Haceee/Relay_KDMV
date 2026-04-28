05e3496595a88ec95c476b260e7eee1937f9cca0
Haceee <59784697+Haceee@users.noreply.github.com>
2026-04-29 01:41:40 +0700

    Update Puller.py

--- Puller.py
+++ Puller.py
@@ -3,17 +3,15 @@ from telegram.ext import Application, MessageHandler, filters
 TOKEN = "8780536402:AAGjzORxNIQKdgV0lpR9E155Jhetein74-U"
 
 async def debug(update, context):
-
     msg = update.message
-
     print("CHAT ID:", msg.chat_id)
-
     print("THREAD ID:", msg.message_thread_id)
-
     print("IS TOPIC:", msg.is_topic_message)
 
-app = Application.builder().token(TOKEN).build()
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.ALL, debug))
+    app.run_polling()
 
-app.add_handler(MessageHandler(filters.ALL, debug))
-
-app.run_polling()
+if __name__ == "__main__":
+    main()
